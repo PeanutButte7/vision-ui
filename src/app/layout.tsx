95874c9f3554e37b251a9f6f37e37be5fa623ce3
PeanutButte7 <adam.barta007@gmail.com>
2023-06-12 16:40:11 +0200

    :chart_with_upwards_trend: Add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react';
+import { Analytics } from '@vercel/analytics/react';
 import { Inter } from 'next/font/google';
 import './globals.css';
 
@@ -12,7 +13,10 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
     return (
         <html lang='en'>
-            <body className={inter.className}>{children}</body>
+            <body className={inter.className}>
+                {children}
+                <Analytics />
+            </body>
         </html>
     );
 }
